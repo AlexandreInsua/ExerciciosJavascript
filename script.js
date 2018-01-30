e003f9543a88e57612645ad3a24bb875ebc95bd2
Alexandre Insua <alexandreinsua@gmail.com>
2018-01-30 19:22:02 +0100

    Exercio boletin 3

--- script.js
+++ script.js
@@ -4,11 +4,6 @@
 var Punto = function (x, y) {
     this.x = x
     this.y = y
-
-    this.mostrarCoordenadas = function () {
-        return "(" + this.x + "," + this.y + ")"
-    }
-
     this.calcularDistancia = function (outroPunto) {
         var dx = outroPunto.x - this.x
         var dy = outroPunto.y - this.y
@@ -19,7 +14,7 @@ var Punto = function (x, y) {
 }
 
 
-function xenerarPuntosAleatorios(densidade = 100, coordenadaXMax, coordenadaYMax) {
+function xenerarPuntosAleatorios(densidade = 20, coordenadaXMax, coordenadaYMax) {
     var puntos = []
     for (i = 0; i < densidade; i++) {
         var aleatorioX = Math.random() * coordenadaXMax
@@ -33,12 +28,12 @@ function xenerarPuntosAleatorios(densidade = 100, coordenadaXMax, coordenadaYMax
 }
 
 function pintarPixel(contexto, x, y, r = 0, g = 0, b = 0, a = 1) {
-    contexto.fillStyle = "rgba(" + r + "," + g + "," + b + "," + a + ")"  // establece el color 
-    contexto.fillRect(x, y, 1, 1)  // dibuja un pixel
+    contexto.fillStyle = "rgba(" + r + "," + g + "," + b + "," + a + ")"  // establece a cor 
+    contexto.fillRect(x, y, 1, 1)  // debuxa un pixel
 }
 
 function dibujaLinea(context, x1, y1, x2, y2, r, g, b, a) {
-    context.strokeStyle = 'rgba(${r}, ${g}, ${b}, ${a})';
+    context.strokeStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
     context.beginPath();
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
@@ -54,62 +49,46 @@ function pintarTodosPixeles(contexto, puntos) {
 
 function conectarConLinhas(contexto, puntos) {
     for (p in puntos) {
-        var numConexions = Math.random() * (4 - 0)
+        var numConexions = Math.random() * 4
         for (var i = 0; i < numConexions; i++) {
             var numAleatorio = parseInt(Math.random() * puntos.lenght)
             var destino = puntos[numAleatorio]
-
-
-            dibujaLinea(contexto, puntos[p].x, puntos[p].y, destino.x, destino.y, 255, 100, 0, 1)
+            dibujaLinea(contexto, puntos[p].x, puntos[p].y, destino.x, destino.y, 255, 0, 0, 0.3)
         }
     }
 }
 
 function conectarConLinhasProximas(contexto, puntos) {
-    for (p in puntos) {
-        var numConexions = Math.random() * (4 - 0)
-        for (var i = 0; i < numConexions; i++) {
-            var numAleatorio = parseInt(Math.random() * puntos.lenght)
-            var destino = puntos[numAleatorio]
-
-
-            dibujaLinea(contexto, puntos[p].x, puntos[p].y, destino.x, destino.y, 255, 100, 0, 1)
-        }
-    }
-}
-
-function obterOsMaisProximos2(contexto, puntos) {
-
-}
-
-function conectarConLinhasProximas(contexto, puntos) {
-    var conexions = []
-    var cont = 0
-
     for (g in puntos) {
+        var cont = 0
         var pu = puntos[g]
         var puntosOrdenados = puntos.sort((a, b) => { pu.calcularDistancia(a) - pu.calcularDistancia(b) })
         for (p in puntosOrdenados) {
-            var numAleatorio = Math.random() * 4
-            if (cont > numConexions) {
+            var numConexions = parseInt(Math.random() * 4)
+            if (cont >= numConexions) {
                 break
             }
-            if (p === g){
-                break
-            }
-            dibujaLinea(contexto, g.x, g.y, p.x, p.y, 255, 100, 50, 0.3)
+            dibujaLinea(contexto, pu.x, pu.y, puntos[p].x, puntos[p].y, corAleatorio(), corAleatorio(), corAleatorio(), alfaAleatorio())
             cont++
         }
     }
 }
 
+function corAleatorio(){
+    return parseInt(Math.random()*255)
+}
+
+function alfaAleatorio(){
+    return Math.random()
+}
 var canvas = document.getElementById("canvas2D")
-var context = canvas.getContext("2d")
+var contexto = canvas.getContext("2d")
 
 var puntos = []
 puntos = xenerarPuntosAleatorios(100, canvas.width, canvas.height)
 
-pintarTodosPixeles(context, puntos)
-conectarConLinhasProximas2(context, puntos)
+
+pintarTodosPixeles(contexto, puntos)
+conectarConLinhasProximas(contexto, puntos)
 
 
